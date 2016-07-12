b45fd88fde774cffd3aa83a4e8f742c2eb772b9f
Priya <priya.pancholi@promactinfo.com>
2016-07-12 18:54:11 +0530

    "some concepts"

---
+++ Concepts/OverrideConcept.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts
+{
+    class OverrideConcept
+    {
+        
+     class Dimension
+        {
+            public const double pi = 3.14;
+            protected int x, y;
+           public  Dimension()
+            {
+
+            }
+
+            public Dimension(int a, int b)
+            {
+                x = a;
+                y = b;
+            }
+
+            public virtual double area()
+            {
+                return x * y;
+            }
+        }
+
+
+
+        class circle:Dimension
+        {
+             public circle(int r): base(r,0)
+            {
+
+            }
+
+            public override double area()
+            {
+                return pi * x * x;
+            }
+
+        }
+
+
+
+        class Square : Dimension
+        {
+            public Square(int l) : base(l, 0)
+            {
+
+            }
+
+            public override double area()
+            {
+                return x*x;
+            }
+
+        }
+
+
+        static void Main()
+        {
+            Dimension c = new circle(7);
+            Dimension s = new Square(3);
+            Console.WriteLine("area of circle is:" + c.area() + "---------area of square is:" + s.area());
+        }
+}
+}
---
+++ Concepts/TryCatch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts
+{
+    class TryCatch
+    {
+
+        static void Main()
+        {
+            int n1,n2,n3;
+            try
+
+            {
+                Console.WriteLine("enter n1 and n2");
+                n1 = Convert.ToInt16(Console.ReadLine());
+                n2 = Convert.ToInt16(Console.ReadLine());
+                if (n2>0)
+                {
+                    n3 = n1 / n2;
+                    Console.WriteLine("your ans is:"+ n3);
+
+                }
+                else
+                {
+                    throw (new DivideByZeroException());
+                }
+            }
+
+
+            catch (DivideByZeroException d)
+            {
+                Console.WriteLine("divide by zero exception");
+
+               
+
+            }
+
+            finally
+            {
+                Console.WriteLine("this is finally block");
+            }
+        }
+    }
+
+
+
+    public class DivideByZeroException : Exception
+
+    {
+
+        public DivideByZeroException()
+
+        { 
+
+        }
+    }
+  }
